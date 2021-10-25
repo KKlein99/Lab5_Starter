ac2d2d6923a187016cbd7a5f41dd3368d2f0ab19
Yu Huang <hyu@ucsd.edu>
2021-10-24 19:14:45 -0700

    start

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,24 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  var horn=document.querySelector('horn');
+  console.log(horn);
+  horn.addEventListener('change',images);
+
+}
+
+function images(){
+  var choice=horn.value;
+  console.log(choice);
+  if(choice==='Air horn'){
+    url(asserts/images/air-horn.svg);
+  }
+  else if(choice==='Car horn'){
+    var img=new img;
+    url(asserts/images/car-horn.svg);
+  }
+  else if(choice==='Party horn'){
+    var img=new img;
+    url(asserts/images/party-horn.svg);
+  }
 }
